1b8b0ae684b0ecb39b82dae55e91fa30c7917e54
stellak-vt <mkang17@vt.edu>
2025-04-20 14:20:56 -0400

    edited structures

--- SimpleRecommenderSystem/src/RuleBasedRecommender.java
+++ SimpleRecommenderSystem/src/RuleBasedRecommender.java
@@ -1,4 +1,15 @@
 
 public class RuleBasedRecommender {
 
+    public RuleBasedRecommender() {
+        // currently empty
+    }
+
+
+    public void recommend(
+        UserProfile user,
+        SinglyLinkedList<Product> products) {
+        // FIXME
+    }
+
 }
---
+++ SimpleRecommenderSystem/src/UserProfile.java
@@ -0,0 +1,36 @@
+
+public class UserProfile {
+
+    private String skinType;
+    private String concern;
+    private int age;
+    private boolean prefersFragranceFree;
+
+    public UserProfile(
+        String skinType,
+        String concern,
+        int age,
+        Boolean prefersFragranceFree) {
+        // constructor
+    }
+
+
+    public String getSkinType() {
+        return ""; // FIXME
+    }
+
+
+    public String getConcern() {
+        return ""; // FIXME
+    }
+
+
+    public int getAge() {
+        return 0; // FIXME
+    }
+
+
+    public boolean prefersFragranceFree() {
+        return false; // FIXME
+    }
+}
